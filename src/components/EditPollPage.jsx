fff6fc2362b6e21db897ed4d2b99ed0934c0b8a4
Diogo Santos <127253177+diogosantos25426@users.noreply.github.com>
2026-02-08 20:36:31 +0000

    Update EditPollPage.jsx

--- src/components/EditPollPage.jsx
+++ src/components/EditPollPage.jsx
@@ -114,7 +114,7 @@ export default function EditPollPage() {
     e.preventDefault();
     try {
       // 1. Atualiza dados da sondagem (Route: PUT /:id)
-      await authFetch(`${API_BASE}/api/polls/${id}`, {
+      await authFetch(`${API_BASE}/api/poll/${id}`, {
         method: "PUT",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ title, description, access, accessCode })
